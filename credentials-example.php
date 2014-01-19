728f87b411d0965e5b9a6661ef7ab43646501ebb
Jason Chavannes <jason.chavannes@gmail.com>
2014-01-19 02:44:12 -0800

    Updating example configs

--- credentials-example.php
+++ credentials-example.php
@@ -5,7 +5,10 @@ define("DATABASE_USER", "root");
 define("DATABASE_PASS", "root");
 define("DATABASE_NAME", "sendstats");
 
-define("STREAM_ADDR", "localhost");
+define("STREAM_ADDR_CLIENT", "localhoste");
+define("STREAM_ADDR_SERVER", "localhost");
+define("STEAM_PORT", 1337);
+define("STREAM_LENGTH", 128000);
 
 define("TOKEN_CODE", "0123456789");
 define("HOST_NAME", "hostname");
